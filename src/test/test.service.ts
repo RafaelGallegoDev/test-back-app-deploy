0ded9c5cdefa861fe6b77ec30966c2b719b4e967
Rafael Gallego <rgallegos-ext@deacero.com>
2026-02-12 14:34:03 -0500

    Description added to return message

--- src/test/test.service.ts
+++ src/test/test.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TestService {
-  getTest(): { status: string; message: string } {
+  getTest(): { status: string; message: string; description: string } {
     return {
       status: 'OK',
       message: 'Test endpoint is working',
+      description:
+        'This is a proove that everything is working between the front-back integration',
     };
   }
 }
